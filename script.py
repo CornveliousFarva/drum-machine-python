426ff50f7739130e4b6ed90f42e9383fcd668025
Greg Schuman <gcschuman@hotmail.com>
2022-10-17 12:25:42 -0700

    Added the colors, screen setup, visible title, and the font.

--- script.py
+++ script.py
@@ -7,3 +7,12 @@ pygame.init()
 WIDTH = 1500
 HEIGHT = 800
 
+# Colors
+black = (0, 0, 0)
+white = (255, 255, 255)
+gray = (128, 128, 128)
+
+# Screen setup, visible title, and font
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+pygame.display.set_caption('Beat Maker')
+label_font = pygame.font.Font('#helvetica.ttf')
